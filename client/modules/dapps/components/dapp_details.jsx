cbf68eb14408333a0830d010eef5fd7f5ae2494f
Jr Reyes <jr.evilboss@gmail.com>
2017-04-28 14:42:23 +0800

    add details to dapp details

--- client/modules/dapps/components/dapp_details.jsx
+++ client/modules/dapps/components/dapp_details.jsx
@@ -9,13 +9,18 @@ class DappDetails extends React.Component {
   render() {
     const {dapp} = this.props;
     return (
-      <div id="details-page" className="section">
+      <div id="details-page" className="section ">
         {(dapp) ?
           <div id="dapp-details-content">
-            <div id="details-page-header" className="card">
+            <div id="details-page-content" className="row">
+              <div className="row container">
+                <div id="details-page-header" className="card transparent">
+                  <span className="card-title white-text"><h4>{dapp.name}</h4>
+                  </span>
+                  <hr/>
+                </div>
+              </div>
 
-            </div>
-            < div id="details-page-content" className="row">
               <div id="details-page-sidebar" className="col s12 m4">
                 <div className="card light-blue">
                   <div className="card-content white-text">
@@ -26,26 +31,27 @@ class DappDetails extends React.Component {
                 <ul id="details-page-about-details" className="collection z-depth-1">
                   <li className="collection-item">
                     <div className="row">
-                      <div className="col s5 grey-text darken-1"><i className="mdi-action-wallet-travel"></i> Name</div>
-                      <div className="col s7 grey-text text-darken-4 right-align">Name</div>
+                      <div className="col s5 grey-text darken-1">
+                        <i className="mdi-action-wallet-travel"></i>URL</div>
+                      <div className="col s7 grey-text text-darken-4 right-align truncate"><a href={dapp.url} target="_blank">{dapp.url}</a></div>
                     </div>
                   </li>
                   <li className="collection-item">
                     <div className="row">
-                      <div className="col s5 grey-text darken-1"><i className="mdi-social-poll"></i> Name</div>
-                      <div className="col s7 grey-text text-darken-4 right-align">Name</div>
+                      <div className="col s5 grey-text darken-1"><i className="mdi-social-poll"></i>Repo</div>
+                      <div className="col s7 grey-text text-darken-4 right-align truncate"><a href={dapp.github} target="_blank">{dapp.github}</a></div>
                     </div>
                   </li>
                   <li className="collection-item">
                     <div className="row">
-                      <div className="col s5 grey-text darken-1"><i className="mdi-social-domain"></i> Name</div>
-                      <div className="col s7 grey-text text-darken-4 right-align">Name</div>
+                      <div className="col s5 grey-text darken-1"><i className="mdi-social-domain"></i> Slack</div>
+                      <div className="col s7 grey-text text-darken-4 right-align truncate"><a href={dapp.slack} target="_blank">{dapp.slack}</a></div>
                     </div>
                   </li>
                   <li className="collection-item">
                     <div className="row">
-                      <div className="col s5 grey-text darken-1"><i className="mdi-social-cake"></i> Name</div>
-                      <div className="col s7 grey-text text-darken-4 right-align">Name</div>
+                      <div className="col s5 grey-text darken-1"><i className="mdi-social-cake"></i> Licence</div>
+                      <div className="col s7 grey-text text-darken-4 right-align">{dapp.license}</div>
                     </div>
                   </li>
                 </ul>
@@ -56,15 +62,14 @@ class DappDetails extends React.Component {
                 </div>
 
               </div>
-              <div id="details-page-content" className="col s12 m8">
-                <span className="card-title white-text"><h4>{dapp.name}</h4></span>
-                <hr/>
-                <div id="details-page-wall-posts" className="row">
+              <div className="col s12 m8">
+
+                <div id="" className="row">
                   <div className="col s12">
-                    <div id="details-page-wall-post" className="card">
+                    <div id="" className="card">
                     </div>
 
-                    <div id="details-page-wall-post" className="card">
+                    <div id="" className="card">
                       <div className="card-details-title">
                         <div className="row">
                           <div className="col s1">
