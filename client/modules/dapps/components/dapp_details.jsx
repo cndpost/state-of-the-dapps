c08470af5f237f6ddc92a797aa7b3ab4ce17f549
Jr Reyes <jr.evilboss@gmail.com>
2017-05-09 00:13:27 +0800

    change to a more elegant looking details page

--- client/modules/dapps/components/dapp_details.jsx
+++ client/modules/dapps/components/dapp_details.jsx
@@ -18,8 +18,7 @@ class DappDetails extends React.Component {
         {(dapp) ?
           <div id="dapp-details-content">
             <div id="details-page-content" className="row">
-
-              <div className="col s12 m8 l8">
+              <div className="col l8 m12 xs12 md6">
                 <div className="row">
                   <div className="col s12">
                     <div id="dapp-details-name" className={`card radius ${dappHelper.getStatusColor(dapp.status)}` }>
@@ -30,7 +29,7 @@ class DappDetails extends React.Component {
                         </div>
                         <div className="col s4"></div>
                         <div className="col s4">
-                          <span className="black-text">
+                          <span className="black-text status-text">
                             {dappHelper.getStatusText(dapp.status)}
                           </span>
                         </div>
@@ -58,110 +57,6 @@ class DappDetails extends React.Component {
                       </div>
 
                       <ul id="overview-collection" className="collection z-depth-1">
-
-                        { dapp.url &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">Site</div>
-                          <div className="col s8 grey-text text-darken-4 left-align truncate">
-                            <a href={dapp.url} target="_blank">
-                              <i className='icon-link fa fa-fw fa-globe'></i> { dapp.url }
-                            </a>
-                          </div>
-                        </li>
-                        }
-
-                        {dapp.github &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">Repository</div>
-                          <div className="col s8 grey-text text-darken-4 left-align truncate">
-                            <a href={dapp.github} target="_blank">
-                              <i className='icon-link fa fa-fw fa-github'></i> { dapp.github }
-                            </a>
-                          </div>
-                        </li>
-                        }
-                        {dapp.blog &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">Blog</div>
-                          <div className="col s8 grey-text text-darken-4 left-align">
-                            <a href={dapp.blog} target="_blank">
-                              <i className='icon-link fa fa-fw fa-book'></i> { dapp.blog }
-                            </a>
-                          </div>
-                        </li>
-                        }
-                        {dapp.wiki &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">Wiki</div>
-                          <div className="col s8 grey-text text-darken-4 left-align">
-                            <a href={dapp.wiki} target="_blank">
-                              <i className='icon-link fa fa-fw fa-wikipedia-w'></i> { dapp.wiki }
-                            </a>
-                          </div>
-                        </li>
-                        }
-                        {dapp.slack &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">
-                            <i className="mdi-social-domain"></i> Slack
-                          </div>
-                          <div className="col s8 grey-text text-darken-4 left-align">
-                            <a href={dapp.slack} target="_blank">
-                              <i className='icon-link fa fa-fw fa-slack'></i> { dapp.slack }
-                            </a>
-                          </div>
-                        </li>
-                        }
-                        {dapp.gitter &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">Gitter</div>
-                          <div className="col s8 grey-text text-darken-4 left-align truncate">
-                            <i className='icon-link fa fa-fw fa-comments'></i> { dapp.gitter }
-                          </div>
-                        </li>
-                        }
-                        { dapp.reddit &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">
-                            Reddit
-                          </div>
-                          <div className="col s8 grey-text text-darken-4 left-align truncate">
-                            <a href={dapp.reddit} target="_blank">
-                              <i className='icon-link fa fa-fw fa-reddit'></i> { dapp.reddit }
-                            </a>
-                          </div>
-                        </li>
-                        }
-                        {dapp.the_etherian &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">The Etherian</div>
-                          <div className="col s8 grey-text text-darken-4 left-align">
-                            <a href={dapp.the_etherian} target="_blank">
-                              <i className='icon-link fa fa-fw fa-newspaper-o'></i> { dapp.the_etherian }
-                            </a>
-                          </div>
-                        </li>
-                        }
-                        { dapp.twitter &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">Twitter</div>
-                          <div className="col s8 grey-text text-darke-4 left-align truncate">
-                            <a href={dapp.twitter} target="_blank">
-                              <i className='icon-link fa fa-fw fa-twitter'></i> { dapp.twitter }
-                            </a>
-                          </div>
-                        </li>
-                        }
-                        { dapp.facebook &&
-                        <li className="collection-item">
-                          <div className="col s4 grey-text darken-1">Facebook</div>
-                          <div className="col s8 grey-text text-darken-4 left-align">
-                            <a href={dapp.facebook} target="_blank">
-                              <i className='icon-link fa fa-fw fa-facebook'></i> { dapp.facebook }
-                            </a>
-                          </div>
-                        </li>
-                        }
                         {dapp.contact &&
                         <li className="collection-item">
                           <div className="col s4 grey-text darken-1">Contact</div>
@@ -217,13 +112,127 @@ class DappDetails extends React.Component {
                           </div>
                         </li>
                         }
+                        <div className="link-title card-details-title">
+                          <div className="row">
+                            <div className="col s4">
+                              <span className="">Important Links</span>
+                            </div>
+                          </div>
+                        </div>
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align truncate">
+                          { dapp.url ?
+                            <a href={dapp.url} target="_blank">
+                              <i className='icon-link fa fa-fw fa-globe'></i> SITE
+                            </a> :
+                            <span><i className='icon-link fa fa-fw fa-globe'></i> NO SITE</span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          {dapp.github ?
+                            <a href={dapp.github} target="_blank">
+                              <i className='icon-link fa fa-fw fa-github'></i> GITHUB
+                            </a> :
+                            <span><i className='icon-link fa fa-fw fa-github'></i> NO GITHUB</span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          {dapp.blog ?
+                            <a href={dapp.blog} target="_blank">
+                              <i className='icon-link fa fa-fw fa-book'></i> BLOG
+                            </a> :
+                            <span><i className='icon-link fa fa-fw fa-book'></i> NO BLOG</span>
+                          }
+                        </div>
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          {dapp.wiki ?
+
+                            <a href={dapp.wiki} target="_blank">
+                              <i className='icon-link fa fa-fw fa-wikipedia-w'></i> WIKI
+                            </a> :
+                            <span>
+                            <i className='icon-link fa fa-fw fa-wikipedia-w'></i> NO WIKI
+                          </span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          {dapp.slack ?
+                            <a href={dapp.slack} target="_blank">
+                              <i className='icon-link fa fa-fw fa-slack'></i> SLACK
+                            </a>
+                            :
+                            <span>
+                              <i className='icon-link fa fa-fw fa-slack'></i> NO SLACK
+                            </span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          {dapp.gitter ?
+                            <a href={dapp.gitter} target="_blank">
+                              <i className='icon-link fa fa-fw fa-comments'></i> GITTER
+                            </a> :
+                            <span><i className='icon-link fa fa-fw fa-comments'></i> NO GITTER</span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          { dapp.reddit ?
+                            <a href={dapp.reddit} target="_blank">
+                              <i className='icon-link fa fa-fw fa-reddit'></i> REDDIT
+                            </a> :
+                            <span><i className='icon-link fa fa-fw fa-reddit'></i> NO REDDIT</span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          {dapp.the_etherian ?
+
+                            <a href={dapp.the_etherian} target="_blank">
+                              <i className='icon-link fa fa-fw fa-newspaper-o'></i> The Etherian
+                            </a> :
+                            <span>
+                            <i className='icon-link fa fa-fw fa-newspaper-o'></i> NO Etherian</span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          { dapp.twitter ?
+                            <a href={dapp.twitter} target="_blank">
+                              <i className='icon-link fa fa-fw fa-twitter'></i> Twitter
+                            </a> :
+                            <span><i className='icon-link fa fa-fw fa-twitter'></i> No Twitter</span>
+                          }
+                        </div>
+
+
+                        <div className="important-link col s6 m4 l4 grey-text text-darken-4 left-align">
+                          { dapp.facebook ?
+                            <a href={dapp.facebook} target="_blank">
+                              <i className='icon-link fa fa-fw fa-facebook'></i> Facebook
+                            </a> :
+                            <span><i className='icon-link fa fa-fw fa-facebook'></i> No Facebook</span>
+
+                          }
+                        </div>
                       </ul>
                     </div>
 
                   </div>
                 </div>
               </div>
-              <div id="details-page-sidebar" className="col s12 m4 l4">
+              <div id="details-page-sidebar" className="col l4 m12 xs12 md6">
                 <div id="related-dapps" className="card radius">
                   <div className="card-details-title">
                     <div className="row">
