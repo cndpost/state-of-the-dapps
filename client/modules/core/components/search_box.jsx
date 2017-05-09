2ee30cf363f40680003a1cba95373a6fc9f2da1b
Jr Reyes <jr.evilboss@gmail.com>
2017-05-10 02:29:44 +0800

    add listener to label

--- client/modules/core/components/search_box.jsx
+++ client/modules/core/components/search_box.jsx
@@ -30,7 +30,7 @@ class SearchBox extends React.Component {
             <div className='input-field col s12'>
               <i className='fa fa-fw fa-search prefix' onClick={this.searchButtonPress.bind(this)}></i>
               <input ref='searchBox' onClick={this.searchButtonPress.bind(this)} onKeyUp={this.handleKeyUp.bind(this)} type='text' className='search-box'></input>
-              <label>Search</label>
+              <label onClick={this.searchButtonPress.bind(this)}>Search</label>
             </div>
             <IconButtons/>
           </div>
