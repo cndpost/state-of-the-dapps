b46ede07e65c1911e09049985c9a926ecb730f89
Jr Reyes <jr.evilboss@gmail.com>
2017-05-27 17:47:07 +0800

    changed to select2

--- client/modules/core/components/search_box.jsx
+++ client/modules/core/components/search_box.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import IconButtons from '../containers/icon_buttions';
+import Select2 from 'react-select2-wrapper';
 
 class SearchBox extends React.Component {
   constructor(props) {
@@ -29,8 +30,19 @@ class SearchBox extends React.Component {
           <div className='search-area'>
             <div className='input-field col s12'>
               <i className='fa fa-fw fa-search prefix' onClick={this.searchButtonPress.bind(this)}></i>
-              <input ref='searchBox' onClick={this.searchButtonPress.bind(this)} onKeyUp={this.handleKeyUp.bind(this)} type='text' className='search-box'></input>
+              <input ref='searchBox' onClick={this.searchButtonPress.bind(this)} onKeyUp={this.handleKeyUp.bind(this)}
+                     type='text' className='search-box'></input>
               <label onClick={this.searchButtonPress.bind(this)}>Search</label>
+              <Select2
+                multiple
+                data={[ 'bug', 'feature', 'documents', 'discussion' ]}
+                options={
+                  {
+                    placeholder: 'search by tags',
+                  }
+                }
+              />
+
             </div>
             <IconButtons/>
           </div>
