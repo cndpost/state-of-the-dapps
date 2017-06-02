be9498150c86cefc179fac72d47290b856510072
Jr Reyes <jr.evilboss@gmail.com>
2017-06-03 03:40:48 +0800

    filter tags based from freq only show greater than 1

--- server/publications/tags.js
+++ server/publications/tags.js
@@ -3,7 +3,9 @@ import {Meteor} from 'meteor/meteor';
 import {check} from 'meteor/check';
 
 export default function () {
-  Meteor.publish('tags', function (tagsId) {
-    return Tags.find(tagsId);
+  Meteor.publish('tags.list', function () {
+    const selector = {freq: {$gt: 1}};
+    const options = {sort: {freq: 1}};
+    return Tags.find(selector,options);
   });
 }
