40101133b17de48f45e8201e2f7d31bee7853848
Jr Reyes <jr.evilboss@gmail.com>
2017-06-03 03:55:48 +0800

    add sort to decending

--- server/publications/tags.js
+++ server/publications/tags.js
@@ -5,7 +5,7 @@ import {check} from 'meteor/check';
 export default function () {
   Meteor.publish('tags.list', function () {
     const selector = {freq: {$gt: 1}};
-    const options = {sort: {freq: 1}};
+    const options = {sort: {freq: -1}};
     return Tags.find(selector,options);
   });
 }
