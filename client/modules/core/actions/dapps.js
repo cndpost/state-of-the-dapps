13c578092ea335f64c6255c7c2f4d0be98f4287c
Jr Reyes <jr.evilboss@gmail.com>
2017-06-13 19:48:39 +0800

    create new action for getting related dapps

--- client/modules/core/actions/dapps.js
+++ client/modules/core/actions/dapps.js
@@ -3,8 +3,17 @@ export default {
     Meteor.call('dapps.count', (err, res) => {
       if (res) {
         LocalState.set('dappCount', res);
-      }else{
-        LocalState.set('dappCount',0);
+      } else {
+        LocalState.set('dappCount', 0);
+      }
+    });
+  },
+  related({Meteor, LocalState}, tags) {
+    Meteor.call('dapps.related', tags, (err, res) => {
+      if (res) {
+        LocalState.set('dappRelated', res);
+      } else {
+        LocalState.set('dappRelated', []);
       }
     });
   }
--- server/methods/dapps.js
+++ server/methods/dapps.js
@@ -6,6 +6,10 @@ export default function () {
   Meteor.methods({
     'dapps.count'() {
       return Dapps.find().count();
+    },
+    'dapps.related'(tags){
+      const selector = (tags) ? {tags: {$in: tags}} : {_id: 'cant-be-found'};
+      return Dapps.find(selector).fetch();
     }
   });
 }
