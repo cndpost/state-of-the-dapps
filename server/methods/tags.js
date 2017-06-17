0cf3ea618a47bff34643155457f9bdcfe76d07c0
Joris Bontje <jorisbontje@gmail.com>
2017-06-17 05:59:11 +0200

    ignore internal / unused fields

--- server/methods/tags.js
+++ server/methods/tags.js
@@ -4,8 +4,9 @@ export default function () {
   Meteor.methods({
     'tags.getAll'() {
       const selector = {freq: {$gt: 1}};
+      const ignoredFields = {last_synced: 0};
       const options = {sort: {freq: -1}};
-      return Tags.find(selector,options).fetch();
+      return Tags.find(selector, ignoredFields, options).fetch();
     }
   });
 }
--- server/publications/dapps.js
+++ server/publications/dapps.js
@@ -3,34 +3,36 @@ import {Meteor} from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
 
 export default function () {
+  const ignoredFields = {last_synced: 0, row_nr: 0, platform: 0, logo: 0};
+
   Meteor.publish('dapps.list', function () {
-    return Dapps.find({}, {sort: {['last_update']: -1}});
+    return Dapps.find({}, ignoredFields, {sort: {['last_update']: -1}});
   }, {
     url: '/api/dapps'
   });
   Meteor.publish('dapp.byTag', function (tag) {
-    return Dapps.find({tags: tag});
+    return Dapps.find({tags: tag}, ignoredFields);
   }, {
     url: '/api/dapps-by-tag/:0'
   });
   Meteor.publish('dapps.byId', function (dappId) {
     const selector = {_id: new Mongo.ObjectID(dappId)};
-    return Dapps.find(selector);
+    return Dapps.find(selector, ignoredFields);
   });
   Meteor.publish('dapps.name', function (dappName) {
     const selector = {name: dappName};
-    return Dapps.find(selector);
+    return Dapps.find(selector, ignoredFields);
   });
   Meteor.publish('dapps.bySlug', function (slug) {
     const selector = {slug};
-    return Dapps.find(selector);
+    return Dapps.find(selector, ignoredFields);
   });
   Meteor.publish('dapps.featured', function () {
     const selector = {tags: {$in: 'featured'}};
-    return Dapps.find(selector);
+    return Dapps.find(selector, ignoredFields);
   });
   Meteor.publish('dapps.related', function (tags) {
     const selector = (tags) ? {tags: {$in: tags}} : {_id: 'cant-be-found'};
-    return Dapps.find(selector);
+    return Dapps.find(selector, ignoredFields);
   });
 }
